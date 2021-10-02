75a64d6a14dd94035e2abb59d0c74e485281deb6
dbudak <dmitriybudak@gmail.com>
2021-10-02 11:23:51 -0500

    init

---
+++ general-info.sh
@@ -0,0 +1,4 @@
+# BUILDS
+
+# Scala projects can be built using Mavent, Ant, Gradle, sbt
+#
---
+++ sbt-project-structure.sh
@@ -0,0 +1,30 @@
+# COMMANDS
+
+sbt exit # gracefully terminate
+sbt about # version
+sbt compile # compiles code
+sbt run # compile and run
+sbt help # man
+sbt clean # delete any files created during compile
+sbt reload # re-eval the config provided to project
+sbt new # create a project using Giter8 template
+sbt console # start REPL
+
+# Giter8 is a framework of templates for skeleton projects
+
+# PROJECT STRUCTURE
+
+# build.sbt - library dependencies
+# PROJECT: - sbt configs
+#  build.properties - sbt version
+#  plugins.sbt - sbt plugins
+#  target - sbt source
+# SRC: - your project source
+#  MAIN: - prod source
+#   SCALA
+#   JAVA
+#   RESOURCES - static files
+#  TEST:
+#   SCALA
+#   RESOURCES - static test files
+# TARGET - compiled code
