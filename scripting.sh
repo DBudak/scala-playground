ffd67bf47c1a9511f917c9ed193410f46c564763
DBudak <dmitriybudak@gmail.com>
2022-07-08 10:31:20 -0500

    add gitignore and scripting

---
+++ scripting.sh
@@ -0,0 +1,8 @@
+# REPL Url: https://ammonite.io/
+# For Windows use WSL
+# For Mac Homebrew to get REPL
+
+# amm - runs REPL
+# amm script.sc - executes script in a REPL
+#   -w          - watch file
+#   --predef    - adds access to functions
