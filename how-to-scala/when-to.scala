1adef248ecc437624cde9c861524de4620a9441f
DBudak <dmitriybudak@gmail.com>
2022-07-19 11:18:34 -0500

    wip

---
+++ how-to-scala/when-to.scala
@@ -0,0 +1,32 @@
+/*
+    Class               |   To represent data with complex computations attached to it
+    Case Class          |   Immutable data representation dependent on its constructor arguments without stateful computations on the inside   
+    Abstract Class      |   To represent "interface" funnel. Single inheritance interface.
+    Trait               |   To represent and enforce common behavior between classes and for multi-inheritance
+    Sealed Trait        |   For fixed number of subclasses
+    ADT                 |   To represent and pattern match though CASES OF something
+    Typeclass           
+    Companion Object
+    Singleton Objects
+    Case Object         |   To create exactly ONE immutable instance of data for all code. Used in SUM ADT.
+    Annotations
+
+
+    List
+    Set
+    Array
+    Map
+    Tuple
+
+    Option
+    Try
+    Either
+
+    
+    Pattern Matching
+    For Comprehension
+
+    Implicit Parameter
+    By-name Parameter
+    Default Parameter
+*/
--- oop/traits.scala
+++ oop/traits.scala
@@ -35,3 +35,45 @@ println(tom.makeSound("gentle"))
 println(tom.move("fast"))
 println(tom.cutenessLevel)
 println(tom.name)
+
+// ===== [In Progress] Finding a dif btw sealed and normal trait
+trait NormalTrait {
+  def sayHello(s:String):String
+  //def sayWhat(s:String):String in normal trait sayWhat needs to be implemented everywhere
+}
+
+case class C1() extends NormalTrait {
+  def sayHello(s:String):String = "Hello"
+}
+
+
+case class C2() extends NormalTrait {
+  def sayHello(s:String):String = "Hola"
+  def sayGoodbye(s:String):String = "Adios!"
+}
+
+// Error, can't pattern match
+
+def patternMatchNormal(o: NormalTrait): String = o match {
+  case C1() => "C1"
+  case C2() => "C2"
+}
+
+
+sealed trait SealedTrait {
+  def sayHello(s:String):String
+}
+
+case class C3() extends SealedTrait {
+  def sayHello(s:String):String = "Hello"
+}
+
+case class C4() extends SealedTrait {
+  def sayHello(s:String):String = "Hola"
+  def sayGoodbye(s:String):String = "Adios!"
+}
+
+def patternMatchSealed(o: SealedTrait): String = o match {
+  case C3() => "C3"
+  case C4() => "C4"
+}
